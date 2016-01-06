c1ea0803c0b0c50e790b392fd90ce7f842bf9a66
zephyr <zephyrjukilo@gmail.com>
2016-01-06 15:34:34 +0100

    Partie 2.2

--- password.py
+++ password.py
@@ -1,4 +1,5 @@
-# coding: utf-8 
+# coding: utf-8
+
 def getNext(password):
     """
     Série de tests exprimés en doctest
@@ -9,27 +10,31 @@ def getNext(password):
     >>> getNext('bc')
     'bd'
     """
-    pwd = list(password)  #1 # On crée un tableau de la taille de la chaine envoyée en argument (password) ( en explosant les caractères ) 
+    pwd = list(password)  #1 # On créé un tableau de la taille de la chaine envoyée en argument (password) ( en explosant les caractères ) 
     found = False
     i=len(pwd)-1
-
-    while not found:
-        if pwd[i] < 'z':
-           pwd[i] = chr(ord(pwd[i])+1)  #2.1 Retourne la lettre en ASCII  du caractère à l'indice i
+    if len(pwd) == 5 and password != 'zzzzz' : # Si le mot de passe est de 5 caractère et que ces caractères sont différents de 'zzzzz'
+        while not found:
+            if pwd[i] < 'z':
+               pwd[i] = chr(ord(pwd[i])+1)  #2.1 Retourne la lettre en ASCII  du caractère à l'indice i
 		   # 2.2  ajoute +1 ,
 		   # 2.3  retourne le nouveau code ASCII en Caractère alpha
-           found = True             
-        else:
-           i = i-1
-           pwd[i+1] = 'a'
+               found = True             
+            else:
+               i = i-1
+               pwd[i+1] = 'a'
     
-    return ''.join(pwd) #3 Assemble le tableau de chaine par des '' et retourne la chaine final
-
+        return ''.join(pwd) #3 Assemble le tableau de chaine par des '' et retourne la chaine finale
+    else:
+        return("La valeur entrée est incorrecte\nUne erreur devra donc être levée ")
 
 
 # Grâce à ce fragment de code, si vous exécutez ce fichier, les tests doctests seront exécutés également. 
 # Si vous ne voulez plus que les tests s'exécutent, commentez les deux lignes ci-dessous. 
 # Si vous préférez lancer vos tests à la main, commentez également les lignes, et utilisez "python -m doctest pass.py" en console. 
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+
+#if __name__ == "__main__":
+#    import doctest
+#    doctest.testmod()
+
+
--- test_password.py
+++ test_password.py
@@ -5,11 +5,16 @@ import password as pwd
 class TestPassword(unittest.TestCase):
 
     def test_getNextNormal(self):
-        self.assertEqual(pwd.getNext("abcd"), "abce")
+        self.assertEqual(pwd.getNext("abcde"), "abcdf")
 
     def test_getNextEndLine(self):
-        self.assertEqual(pwd.getNext("abhz"), "abia")
+        self.assertEqual(pwd.getNext("abhzv"), "abhzw")
+        
+    def test_getNext_zzzzz(self):
+        self.assertEqual(pwd.getNext("zzzzz"), "La valeur entrée est incorrecte\nUne erreur devra donc être levée ")
 
-
+    def test_getNext_password(self):
+        self.assertEqual(len("likol"), 5)
+        
 # Permet d'exécuter les tests si ce fichier est exécuté
 unittest.main()
